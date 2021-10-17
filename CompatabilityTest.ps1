8ab36519ed7c7c86a46319afd38cdcb03c3ef7ef
dmankl <66547321+dmankl@users.noreply.github.com>
2021-10-17 12:27:28 -0700

    Changed test
    Updated

--- CompatabilityTest.ps1
+++ CompatabilityTest.ps1
@@ -7,6 +7,7 @@ If (!( Test-Path -Path $GPU )) {
     $Output = "$GPU"
     Invoke-WebRequest -Uri $Url -OutFile $Output
 }
+$Compatible = "No"
 $GPUs = Import-Csv -Path $GPU 
 $graphics = (Get-CimInstance win32_VideoController).VideoProcessor
 if ($graphics.Length -gt 1) {
@@ -19,7 +20,9 @@ if ($graphics.Length -gt 1) {
         }
     }
 }
-if ($Compatible -ne "Yes") {
+if ($Compatible -eq "No") {
     Read-Host "It seems you do not have a compatible CPU/GPU to convert to HEVC, Exiting."
     Break
+}elseif ($Compatible -eq "Yes") {
+    Read-Host "Congratulations! You can convert videos!"
 }
