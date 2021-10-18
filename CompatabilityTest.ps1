089ecc73b9548d7233ecb337810d1a4c8e236fd7
dmankl <66547321+dmankl@users.noreply.github.com>
2021-10-17 20:40:35 -0700

    Updated Lists

--- CompatabilityTest.ps1
+++ CompatabilityTest.ps1
@@ -1,20 +1,26 @@
 ﻿#Future addition- check if your CPU/GPU Is capable of transcoding.
-$FFMPEG = "C:\FFMPEG"
-$Resources = "C:\FFMPEG\_Conversion"
-If (!( Test-Path -Path $Resources )) { New-Item -Path $FFMPEG -Name "_Conversion" -ItemType "Directory" }   
-$GPU = "$Resources\GPU.csv"
-If (!( Test-Path -Path $GPU )) { 
+function Confirm-CompatibleHardwareEncoder {
+    $Resources = "C:\FFMPEG\_Conversion"
+    If (!( Test-Path -Path $Resources )) { New-Item -Path $FFMPEG -Name "_Conversion" -ItemType "Directory" }   
     $Url = "https://raw.githubusercontent.com/dmankl/HomeCode/master/GPU.csv"
-    Invoke-WebRequest -Uri $Url -OutFile $GPU
-}
-$GPUs = Import-Csv -Path $GPU 
-ForEach ($Graphic in (Get-CimInstance win32_VideoController).VideoProcessor) {
-    if ($GPUs.gpu -contains $Graphic) {
-        $Compatible = "Yes"
+    $GPUs = Invoke-WebRequest -Uri $Url -UseBasicParsing | ConvertFrom-Csv
+    $graphicsCards = @(Get-CimInstance win32_VideoController)
+    $supportedGPU = @()
+    ForEach ($Graphic in $graphicsCards) {
+        if ($GPUs.gpu -contains $Graphic.VideoProcessor) {
+            $supportedGPU += $Graphic
+        }
+    }
+    
+    if ($supportedGPU.count -ge 1) {
+        return $true
+    }
+    else {
+        return $false
     }
 }
 
-if ($Compatible -eq "Yes") {
+if (Confirm-CompatibleHardwareEncoder) {
     Read-Host "Congratulations! You can convert videos!"
 }else {
     Read-Host "It seems you do not have a compatible CPU/GPU to convert to HEVC, Exiting."
